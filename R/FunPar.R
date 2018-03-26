1497c0aa14de08a63b9ed7394d6e9e6bfad4a6b2
Reto <retozihlmann@outlook.com>
2018-03-26 17:23:37 +0200

    improve help file of FunPar

--- R/FunPar.R
+++ R/FunPar.R
@@ -2,13 +2,15 @@
 #'
 #' Set parameters (par) in R to favorit default values. Returns previous par values
 #' invisible.
-#' @param mar smaller plot margin
-#' @param xaxs do not add 4 percent to x axis' lim
-#' @param yaxs do not add 4 percent to y axis' lim
-#' @param tcl ticks look inside
-#' @param las turn y axis labels
-#' @param mgp position of axis titles and labels in lines
-#' @param lab preferd number of ticks (4 instead of 5)
+#' @param mar smaller plot margin. default c(5.1, 4.1, 4.1, 2.1))
+#' @param xaxs do not add 4 percent to x axis' lim. default "r"
+#' @param yaxs do not add 4 percent to y axis' lim. default "r"
+#' @param tcl ticks look inside. default -0.5
+#' @param las turn y axis labels. default 0
+#' @param mgp position of axis titles and labels in lines. default c(3, 1, 0)
+#' @param lab preferd number of ticks (4 instead of 5). default c(5, 5, 7)
+#' @param family prefered font family (Segoe). default ""
+#' @param cex prefered character expansion factor. default 1
 #' @return invisible original par values
 #' @author Reto Zihlmann
 #' @seealso par()
